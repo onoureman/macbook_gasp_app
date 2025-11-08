46e4d09da46a499da70f0789bc568598ca541111
onoureman <emansd8@gmail.com>
2025-11-08 14:24:42 +0200

    implement features section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useRef } from 'react'
+import {useEffect, useRef} from "react";
 
 const Hero = () => {
+    const videoRef = useRef();
 
-   const videoRef = useRef(null);
+    useEffect(() => {
+        if(videoRef.current) videoRef.current.playbackRate = 2;
+    }, []);
 
-   useEffect(() => {
-    if (videoRef.current) videoRef.current.playbackRate = 2;
-  }, []);
-  return (
-    <section id='hero'>
-        <div>
-            <h1>MacBook Pro</h1>
-            <img src='/title.png' alt='MacBook Pro' />
-        </div>
+    return (
+        <section id="hero">
+            <div>
+                <h1>MacBook Pro</h1>
+                <img src="/title.png" alt="MacBook Title" />
+            </div>
 
-        <video ref={videoRef} src='/videos/hero.mp4' autoPlay muted playsInline />
+            <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
 
-        <button>Buy</button>
-        <p>From $1999.00 or $83.29/mo for 24 months</p>
-    </section>
-  )
+            <button>Buy</button>
+
+            <p>From $1599 or $133/mo for 12 months</p>
+        </section>
+    )
 }
-
 export default Hero
